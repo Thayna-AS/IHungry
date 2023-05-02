90f6050b5a1c9364e739de761691b1152890a633
Thayna-AS <thayna.almeida.silva23@gmail.com>
2023-05-02 19:38:10 -0300

    Update

--- ProjetoGuia/src/domain/model/PrintTela.java
+++ ProjetoGuia/src/domain/model/PrintTela.java
@@ -15,8 +15,10 @@ public class PrintTela {
 
 	public static void MenuInicio() {
 
+		
+		//print Menu incial.
 		System.out.println(TEXT_YELLOW + ANSI_BLACK_BACKGROUND
-				+ "-------------------------------------------------------------- ");
+				         + "-------------------------------------------------------------- ");
 		System.out.println("                  BEM-VINDO AO I HUNGRY                        ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println(" _____________________________________________________________ ");
@@ -30,28 +32,28 @@ public class PrintTela {
 		System.out.println("|       7 - Sair do cardapio                                  |");
 		System.out.println("|                                                             |");
 		System.out.println("---------------------------------------------------------------");
-		System.out.println("          Digite a opção desejada                              ");
+		System.out.println("                 Informe a opção desejada                      ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("" + TEXT_RESET);
-	}
+	}		
 
-	/* print da ária de lanches */
+	/* print da área de lanches. */
 	public static void EscolhaLanches() {
 		System.out.println(TEXT_YELLOW + ANSI_BLACK_BACKGROUND
-				+ "---------------------------------------------------------------");
-		System.out.println("                   ESCOLHA SEU LANCHE                          ");
+				         + "---------------------------------------------------------------");
+		System.out.println("                     ESCOLHA O LANCHE                          ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println(" _____________________________________________________________ ");
 		System.out.println("|                                                             |");
-		System.out.println("|   5 CHICKEN BARBECUE BURGER-------------avulso R$ 24,90     |");
-		System.out.println("|   6 CLASSIC BACON BURGER ---------------avulso R$ 29,90     |");
-		System.out.println("|   7 CHEESE BACON BURGER ----------------avulso R$ 27,90     |");
-		System.out.println("|   8 BIB´S BURGER PLANT -----------------avulso R$ 24,90     |");
-		System.out.println("|   9 TRADICIONAL ROSBIFE ----------------avulso R$ 28,90     |");
+		System.out.println("|   5 CHICKEN BARBECUE BURGER-------------Unidade R$ 24,90    |");
+		System.out.println("|   6 CLASSIC BACON BURGER ---------------Unidade R$ 29,90    |");
+		System.out.println("|   7 CHEESE BACON BURGER ----------------Unidade R$ 27,90    |");
+		System.out.println("|   8 BIB´S BURGER PLANT -----------------Unidade R$ 24,90    |");
+		System.out.println("|   9 TRADICIONAL ROSBIFE ----------------Unidade R$ 28,90    |");
 		System.out.println("|_____________________________________________________________|");
 		System.out.println("                                                               ");
 		System.out.println("---------------------------------------------------------------");
-		System.out.println("                   Digite a opção desejada                     ");
+		System.out.println("                 Informe a opção que deseja                    ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("" + TEXT_RESET);
 	}
@@ -59,19 +61,19 @@ public class PrintTela {
 	public static void EscolhaEntrada() {
 
 		System.out.println(TEXT_YELLOW + ANSI_BLACK_BACKGROUND
-				+ "---------------------------------------------------------------");
-		System.out.println("                   ESCOLHA SUA ENTRADA                         ");
+				         + "---------------------------------------------------------------");
+		System.out.println("                   ESCOLHA A ENTRADA                           ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println(" _____________________________________________________________ ");
 		System.out.println("|                                                             |");
-		System.out.println("|   1 Coxinha de frango-------------------avulso R$ 8,00      |");
-		System.out.println("|   2 Pastel de queijo--------------------avulso R$ 12,00     |");
-		System.out.println("|   3 Pastel de carne --------------------avulso R$ 12,00     |");
-		System.out.println("|   4 Pão de queijo ----------------------avulso R$ 5,00      |");
+		System.out.println("|   1 Coxinha de frango-------------------Unidade R$ 8,00     |");
+		System.out.println("|   2 Pastel de queijo--------------------Unidade R$ 12,00    |");
+		System.out.println("|   3 Pastel de carne --------------------Unidade R$ 12,00    |");
+		System.out.println("|   4 Pão de queijo ----------------------Unidade R$ 5,00     |");
 		System.out.println("|_____________________________________________________________|");
 		System.out.println("                                                               ");
 		System.out.println("---------------------------------------------------------------");
-		System.out.println("                   Digite a opção desejada                     ");
+		System.out.println("                 Informe a opção que deseja                    ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("" + TEXT_RESET);
 
@@ -80,35 +82,26 @@ public class PrintTela {
 	public static void EscolhaSobremesa() {
 
 		System.out.println(TEXT_YELLOW + ANSI_BLACK_BACKGROUND
-				+ "---------------------------------------------------------------");
+				         + "---------------------------------------------------------------");
 		System.out.println("                   ESCOLHA SUA SOBREMESA                       ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println(" _____________________________________________________________ ");
 		System.out.println("|                                                             |");
-		System.out.println("|  17 Petit Gateau -----------------------avulso R$ 22,00     |");
-		System.out.println("|        Pequeno bolo de chocolate com casca e recheio        |");
-		System.out.println("|        cremoso servido com sorvete de creme                 |");
-		System.out.println("|                                                             |");
-		System.out.println("|  18 Torta de Maçã ----------------------avulso R$ 25,00     |");
-		System.out.println("|        Deliciosa torta de Maçã Servida com Sorvete          |");
-		System.out.println("|        de Canela                                            |");
-		System.out.println("|                                                             |");
-		System.out.println("|  19 Açai -------------------------------avulso R$ 17,00     |");
-		System.out.println("|   1 Petit Gateau -----------------------Avulso R$ 22,00     |");
+		System.out.println("|  17 Petit Gateau -----------------------Unidade R$ 22,00    |");
 		System.out.println("|        Pequeno bolo de chocolate, com casca e recheio       |");
 		System.out.println("|        cremoso, servido com sorvete de creme.               |");
 		System.out.println("|                                                             |");
-		System.out.println("|   2 Torta de Maçã ----------------------Avulso R$ 25,00     |");
+		System.out.println("|  18 Torta de Maçã ----------------------Unidade R$ 25,00    |");
 		System.out.println("|        Deliciosa torta de Maçã, servida com Sorvete         |");
 		System.out.println("|        de Canela.                                           |");
 		System.out.println("|                                                             |");
-		System.out.println("|   3 Açai -------------------------------Avulso R$ 17,00     |");
-		System.out.println("|         Açai na taça com acompanhamentos à escolha          |");
+		System.out.println("|  19 Açai -------------------------------Unidade R$ 17,00    |");
+		System.out.println("|         Açai na taça, com acompanhamentos à escolha         |");
 		System.out.println("|         do cliente.                                         |");
 		System.out.println("|_____________________________________________________________|");
 		System.out.println("                                                               ");
 		System.out.println("---------------------------------------------------------------");
-		System.out.println("                   Digite a opção desejada                     ");
+		System.out.println("                   Informe a opção que deseja                  ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("" + TEXT_RESET);
 
@@ -117,35 +110,35 @@ public class PrintTela {
 	public static void EscolhaBebida() {
 
 		System.out.println(TEXT_YELLOW + ANSI_BLACK_BACKGROUND
-				+ "---------------------------------------------------------------");
-		System.out.println("                   ESCOLHA SUA BEBIDA                          ");
+				         + "---------------------------------------------------------------");
+		System.out.println("                     ESCOLHA A BEBIDA                          ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println(" _____________________________________________________________ ");
 		System.out.println("|                                                             |");
-		System.out.println("|   10 Água 650ml -------------------------avulso R$ 5,00     |");
-		System.out.println("|   11 Chá matte --------------------------avulso R$ 5,00     |");
-		System.out.println("|   12 Refrigerante lata 350ml ------------avulso R$ 7,00     |");
+		System.out.println("|   10 Água 650ml -------------------------Unidade R$ 5,00    |");
+		System.out.println("|   11 Chá matte --------------------------Unidade R$ 5,00    |");
+		System.out.println("|   12 Refrigerante lata 350ml ------------Unidade R$ 7,00    |");
 		System.out.println("|_____________________________________________________________|");
 		System.out.println("---------------------------------------------------------------");
-		System.out.println("                   BEBIDAS ALCOÓLICAS                          ");
+		System.out.println("                     BEBIDAS ALCOÓLICAS                        ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println(" _____________________________________________________________ ");
 		System.out.println("|                                                             |");
-		System.out.println("|   13 Heineken 600ml ---------------------avulso R$ 15,00    |");
-		System.out.println("|   14 Brahma 600ml -----------------------avulso R$ 12,00    |");
-		System.out.println("|   15 Skol 600ml -------------------------avulso R$ 10,00    |");
-		System.out.println("|   16 Colmeia Drinks ---------------------avulso R$ 10,00    |");
+		System.out.println("|   13 Heineken 600ml ---------------------Unidade R$ 15,00   |");
+		System.out.println("|   14 Brahma 600ml -----------------------Unidade R$ 12,00   |");
+		System.out.println("|   15 Skol 600ml -------------------------Unidade R$ 10,00   |");
+		System.out.println("|   16 Colmeia Drinks ---------------------Unidade R$ 10,00   |");
 		System.out.println("|_____________________________________________________________|");
 		System.out.println("                                                               ");
 		System.out.println("---------------------------------------------------------------");
-		System.out.println("                   Digite a opção desejada                     ");
+		System.out.println("                   Digite a opção que deseja                   ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("" + TEXT_RESET);
 	}
 
 	public static void EscolhaQuantidade() {
 		System.out.println(TEXT_YELLOW + ANSI_BLACK_BACKGROUND
-				+ "---------------------------------------------------------------");
+				         + "---------------------------------------------------------------");
 		System.out.println("                     Escolha a quantidade                      ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("" + TEXT_RESET);
@@ -153,7 +146,7 @@ public class PrintTela {
 
 	public static void AdicionarFinalizar() {
 		System.out.println(TEXT_YELLOW + ANSI_BLACK_BACKGROUND
-				+ "---------------------------------------------------------------");
+				         + "---------------------------------------------------------------");
 		System.out.println("       Gostaria de adicionar outro item dessa categoria?       ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("                          1 - Não                              ");
@@ -164,8 +157,8 @@ public class PrintTela {
 
 	public static void confirma() {
 		System.out.println(TEXT_YELLOW + ANSI_BLACK_BACKGROUND
-				+ "---------------------------------------------------------------");
-		System.out.println("                CATECORIA RETIRADA COM SUCESSO                 ");
+				         + "---------------------------------------------------------------");
+		System.out.println("                Categoria retirada com sucesso!                ");
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("" + TEXT_RESET);
 	}
